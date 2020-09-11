43c7e9af2a78dd23f8b82e90c378ad9792e6cff0
Colin Gravill <colin@gravill.com>
2020-09-11 15:30:12 +0100

    Create a new notebook that loads with .NET Interactive

---
+++ REInteractiveAPI/REIL.fs
@@ -0,0 +1,29 @@
+﻿namespace Microsoft.Research.RENotebook
+
+//methods that manipulate REIL structures
+type REIL = 
+    static member Load problemString = 
+        problemString |>  Microsoft.Research.ReasoningEngine.Model.Parse
+
+    static member LoadFile (problemFilePath:string) = 
+        let problemString = System.IO.File.ReadAllText problemFilePath
+        Microsoft.Research.ReasoningEngine.Model.Parse(problemString)
+
+    static member Check problem  = 
+        Microsoft.Research.ReasoningEngine.Solver.BMC problem  
+    
+    static member CheckAndPrint model = 
+        let sol = model |> REIL.Check
+        if sol.IsSome then
+            "Solution(s) found"
+        else
+            "No solution(s) found"
+
+    static member Enumerate n problem  = 
+        Microsoft.Research.ReasoningEngine.Solver.BMCIterN n problem              
+
+    //Experimental methods
+    static member Infer problem  = 
+        Microsoft.Research.ReasoningEngine.Inference.BMCInference problem  
+
+  
---
+++ REInteractiveAPI/ReLoad.fsx
@@ -0,0 +1,20 @@
+﻿module ReasoningEngine
+
+#r @"./bin/Release/netstandard2.0/ReasoningEngine.dll"
+#r @"./bin/Release/netstandard2.0/REInteractiveApi.dll"
+
+open Microsoft.Research.RENotebook
+
+type ReilAPI = Microsoft.Research.RENotebook.REIL
+module Cst = Microsoft.Research.ReasoningEngine.Constraint
+module Var = Microsoft.Research.ReasoningEngine.Var
+type TrajVis = Microsoft.Research.RENotebook.Lib.TrajectoryVisualization
+printfn "Loading the Reasoning Engine (RE)..."
+
+Formatter.Register<Microsoft.Research.RENotebook.Lib.PlotlyOutput>(
+    mimeType = "text/html",
+    formatter = Func<_,_,_,_>(fun context person (writer: TextWriter) ->
+        match person with
+        | Microsoft.Research.RENotebook.Lib.PlotlyOutput.Chart plot -> display(plot) |> ignore
+        | Microsoft.Research.RENotebook.Lib.PlotlyOutput.Message stuff -> display(stuff) |> ignore
+        true))
